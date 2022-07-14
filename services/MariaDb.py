58504c51679f4071ff768a71e1fd16b710725f61
bruno <brunoramos013@gmail.com>
2022-07-14 15:16:40 -0300

    fix query when accountID is set

--- services/MariaDb.py
+++ services/MariaDb.py
@@ -34,9 +34,14 @@ class MariaDb:
         return conn.cursor()
 
     def get_host_names(self):
+        query ="SELECT domain.url, domain.account_id, domain.id, domain.video_date FROM domain INNER JOIN auto_ad ON auto_ad.domain_id=domain.id and auto_ad.name = 'slider' GROUP BY domain.id;"
+        
+        if(os.getenv("ACCOUNT_ID")):
+            account_id = os.getenv("ACCOUNT_ID")
+            query = f"SELECT domain.url, domain.account_id, domain.id, domain.video_date FROM domain INNER JOIN auto_ad ON auto_ad.domain_id=domain.id and auto_ad.name = 'slider' WHERE domain.account_id = '{account_id}' GROUP BY domain.id;"
+        
         host_names = []
-        self.connection.execute(
-            "SELECT domain.url, domain.account_id, domain.id, domain.video_date FROM domain INNER JOIN auto_ad ON auto_ad.domain_id=domain.id and auto_ad.name = 'slider' GROUP BY domain.id;")
+        self.connection.execute(query)
         for url, account_id, id, video_date in self.connection:
             host_names.append({account_id: {'url': url, 'id': id, 'video_date': video_date}})
 
